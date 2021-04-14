5d29fd523ff00f6000296b4257b6cbc0738169af
Jeffery <522023893@qq.com>
2021-04-14 11:47:37 +0800

    登录，找回密码更改密码

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+ before_action :set_current_user
+ #所有其他action之前， index，create之类
+    def set_current_user
+        if session[:user_id]
+            Current.user = User.find_by(id: session[:user_id])
+            #find_by 不会报错
+        end
+    end
+
+    def require_uer_logged_in
+        redirect_to sign_in_path, alert: "You must be signed in to do that." if Current.user.nil?
+    end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,5 @@
 class MainController < ApplicationController
 
-    def index
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-            #find_by 不会报错
-        end
-    end
+    
 
 end
